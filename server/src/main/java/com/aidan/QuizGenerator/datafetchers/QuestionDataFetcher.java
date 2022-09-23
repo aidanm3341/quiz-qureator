0a14c9c9fd9495d95d62464eb31f1d25b489b10c
Aidan McPhelim <aidanmcp@hotmail.co.uk>
2022-09-23 15:58:40 +0100

    typo in question

--- server/src/main/java/com/aidan/QuizGenerator/datafetchers/QuestionDataFetcher.java
+++ server/src/main/java/com/aidan/QuizGenerator/datafetchers/QuestionDataFetcher.java
@@ -5,6 +5,8 @@ import com.aidan.QuizGenerator.model.Question;
 import com.aidan.QuizGenerator.model.TextQuestion;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,15 +16,18 @@ import java.util.stream.Collectors;
 @DgsComponent
 public class QuestionDataFetcher {
 
+    Logger logger = LoggerFactory.getLogger(QuestionDataFetcher.class);
+
     private final List<Question> questions = List.of(
             new TextQuestion("What is the capital of Afghanistan", "Kabul"),
-            new TextQuestion("Who wrote the Ender's Game saga?", "Orsen Scott Card"),
+            new TextQuestion("Who wrote the Ender's Game saga?", "Orson Scott Card"),
             new TextQuestion("Who played Mrs.Weasley in Harry Potter", "Julie Walters"),
             new PictureQuestion("Who is this character?", "Tony the Tiger", "tony.jpeg")
     );
 
     @DgsQuery
     public List<Question> questions(Integer amount) {
+        logger.info("Call made to 'questions' endpoint");
         if(amount == null)
             return questions;
 
