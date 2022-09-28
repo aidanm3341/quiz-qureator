edf3d0b6e0514a93551ea4d07ff2301622139003
Aidan McPhelim <aidanmcp@hotmail.co.uk>
2022-09-28 18:42:47 +0100

    added color changing background

--- client/src/app/questions/questions.component.ts
+++ client/src/app/questions/questions.component.ts
@@ -18,6 +18,10 @@ export class QuestionsComponent implements OnInit {
     questions: Question[] = [];
     error: string = "";
     serverUrl: string = "http://localhost:8080/";
+
+    r: number = 255;
+    g: number = 255;
+    b: number = 255;
     
     constructor(private apollo: Apollo) { }
     
@@ -37,8 +41,17 @@ export class QuestionsComponent implements OnInit {
             this.questions = data.questions;
             console.log(this.questions)
 
+            this.r = this.randomIntFromInterval(150, 230);
+            this.g = this.randomIntFromInterval(150, 230);
+            this.b = this.randomIntFromInterval(150, 230);
+
             this.error = error;
         })
     }
+
+    randomIntFromInterval(min: number, max: number): number { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+      
     
 }
